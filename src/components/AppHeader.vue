98fdaf362d73682a593498c8d8381b79c362fbde
Domenico-lel <domenicolella2@gmail.com>
2024-01-11 12:42:46 +0100

    inizializzazione progetto

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+import AppSearch from "./AppSearch.vue";
+export default {
+    name: "AppHeader",
+    components: { AppSearch },
+    data() {
+        return {};
+    },
+    methods: {
+        testMess(parametro, num) {
+            console.log("hey", parametro, num);
+        },
+    },
+};
+</script>
+<template>
+    <AppSearch message="ciao" @eventodalfiglio="testMess" />
+</template>
+<style scoped></style>
