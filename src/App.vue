928de6534f3b10c4eb6fbc9457581863a39b986a
Domenico-lel <domenicolella2@gmail.com>
2024-01-12 18:19:47 +0100

    add film in page - lingue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
 <script>
+import axios from "axios";
+
 import AppHeader from "./components/AppHeader.vue";
-import AppSearch from "./components/AppSearch.vue";
+import AppMain from "./components/AppMain.vue";
+import { store } from "./store";
 export default {
   name: "App",
-  components: { AppHeader, AppSearch },
+  components: { AppHeader, AppMain },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    handleSearchEvent() {
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            api_key: "0b1d9ed99a9aca1ff9756c3ae887b688",
+            query: this.store.searchText,
+            language: "it-IT",
+          },
+        })
+        .then((response) => {
+          this.store.movies = response.data.results;
+          console.log(response);
+          this.store.movies = response.data.results;
+        });
+    },
+  },
+  created() { },
 };
 </script>
 
 <template>
-  <AppHeader />
+  <AppHeader @performSearch="handleSearchEvent" />
+  <AppMain />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppMain.vue
@@ -0,0 +1,35 @@
+<script>
+import { store } from "../store";
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            store,
+        };
+    },
+    props: {},
+    methods: {
+        getFlag(lang) {
+            if (lang == "en") {
+                lang = "uk";
+            } else if (lang == "pt") {
+                lang = "po";
+            }
+            const flag = `https://www.worldometers.info//img/flags/small/tn_${lang}-flag.gif`;
+            return flag;
+        },
+    },
+};
+</script>
+<template>
+    <main>
+        <div v-for="movie in store.movies">
+            <h2>{{ movie.title }}</h2>
+            <h5>{{ movie.original_title }}</h5>
+            <img :src="getFlag(movie.original_language)" />
+            <p>{{ movie.original_language }}</p>
+            <p>{{ movie.vote_average }}</p>
+        </div>
+    </main>
+</template>
+<style scoped></style>
