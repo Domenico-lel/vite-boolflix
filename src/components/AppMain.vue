9d0b2b5647b63540e84941b5f84d214ac96363a1
Domenico-lel <domenicolella2@gmail.com>
2024-01-14 03:09:24 +0100

    aggiunti img film e stelle

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,17 +18,51 @@ export default {
             const flag = `https://www.worldometers.info//img/flags/small/tn_${lang}-flag.gif`;
             return flag;
         },
+        getImage(img) {
+            let url = "https://image.tmdb.org/t/p/" + "w342" + img;
+            return url;
+        },
+        calcoloVoto(voto) {
+            let votoUno = (voto / 10) * 100;
+            let votoDue = Math.round(votoUno / 100) * 5;
+            return votoDue;
+        },
     },
 };
 </script>
 <template>
     <main>
+        <h2>Film</h2>
+
         <div v-for="movie in store.movies">
+            <img :src="getImage(movie.poster_path)" />
             <h2>{{ movie.title }}</h2>
             <h5>{{ movie.original_title }}</h5>
             <img :src="getFlag(movie.original_language)" />
-            <p>{{ movie.original_language }}</p>
-            <p>{{ movie.vote_average }}</p>
+            <!-- <p>{{ movie.original_language }}</p> -->
+            <div>
+                <span v-for="votoSingolo in calcoloVoto(movie.vote_average)"> ★ </span>
+                <span v-for="votoSingolo in 5 - calcoloVoto(movie.vote_average)">
+                    ☆
+                </span>
+            </div>
+        </div>
+
+        <h2>Serie TV</h2>
+        <div v-for="singleSeries in store.series">
+            <img :src="getImage(singleSeries.poster_path)" />
+            <h2>{{ singleSeries.name }}</h2>
+            <h5>{{ singleSeries.original_name }}</h5>
+            <img :src="getFlag(singleSeries.original_language)" />
+            <!-- <p>{{ singleSeries.original_language }}</p> -->
+            <div>
+                <span v-for="votoSingolo in calcoloVoto(singleSeries.vote_average)">
+                    ★
+                </span>
+                <span v-for="votoSingolo in 5 - calcoloVoto(singleSeries.vote_average)">
+                    ☆
+                </span>
+            </div>
         </div>
     </main>
 </template>
